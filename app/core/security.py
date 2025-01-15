f070240c91f6ec1c2c2b395a3e6246019ab1915b
Duilio Restuccia <restucciaduilio@gmail.com>
2025-01-15 18:08:57 -0500

    PyJWT decode
    PyJWT decode

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from typing import Optional
-import jwt
+import jwt as pyjwt
+from jose import jwt
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from app.core.config import settings
@@ -25,7 +26,7 @@ class SecurityManager:
     def decode_token(self, token: str):
         try:
             print("Starting token decode...")
-            payload = jwt.decode(token, self.secret_key, algorithms=["HS256"])
+            payload = pyjwt.decode(token, self.secret_key, algorithms=["HS256"])
             print("Decoded payload:", payload)
             if not isinstance(payload.get('sub'), str):
                 raise ValueError("Subject must be a string")
